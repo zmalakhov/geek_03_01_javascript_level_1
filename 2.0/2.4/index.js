22b7d9de8bedca7e02e90f9614c084f0bbdc8270
zmalakhov <zm.13.15.zm@gmail.com>
2019-10-19 19:32:16 +0300

    015 - 2.4

---
+++ 2.0/2.4/index.js
@@ -0,0 +1,27 @@
+
+const a =1;
+
+switch (a) {
+    case 1:
+        // body
+        break;
+    case 2:
+        // body
+        break;
+    default:
+        // body
+        break;
+}
+const tel = "89252346578";
+switch (tel[0]) {
+    case "8":
+        console.log("все верно");
+        break;
+    case "7":
+        console.log("вы ввели 7");
+        break;
+    default:
+        console.log("неправильно");
+        break;
+}
+
