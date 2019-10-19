ad334c3b49562c82d6fbd4c39a648889d62921a2
zmalakhov <zm.13.15.zm@gmail.com>
2019-10-19 19:04:05 +0300

    013 - 2.2

---
+++ 2.0/2.2/index.js
@@ -0,0 +1,11 @@
+const bool = true;
+if(bool){
+    // тело
+}
+const tel = "89252346578";
+if (tel.length == 11){
+    console.log("Тест 1 пройден");
+    if (tel[0] == "8"){
+        console.log("Тест 2 пройден");
+    }
+}
