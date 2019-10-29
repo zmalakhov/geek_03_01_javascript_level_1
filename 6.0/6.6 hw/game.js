1e8834a2a77e45a35a3f762751278e2bfd0809c5
zmalakhov <zm.13.15.zm@gmail.com>
2019-10-29 07:38:19 +0300

    049 - 6.6 - JS_HW_06

---
+++ 6.0/6.6 hw/game.js
@@ -0,0 +1,35 @@
+class Bird {
+    constructor(name) {
+        this.name = name;
+        this.points = 0;
+        this.wasEaten = false;
+    }
+}
+
+const birds = [];
+
+for (let i = 1; i <= 10; i++) {
+    let bird = new Bird("Bird " + i);
+    //console.log(bird);
+    birds.push(bird);
+}
+
+console.log(birds);
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+do {
+    let indexBird = getRandomInt(0, birds.length-1);
+    let indexBirdToEat = indexBird === 0 ? indexBird+1 : indexBird-1;
+
+    birds[indexBird].points++;
+    birds[indexBirdToEat].wasEaten = true;
+    birds.splice(indexBirdToEat, 1);
+} while (birds.length > 1);
+
+console.log(birds);
+
+
+
