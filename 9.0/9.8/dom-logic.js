aa5b8129a557068582aaabf08678101eb8107dc4
zmalakhov <zm.13.15.zm@gmail.com>
2019-11-12 05:57:47 +0300

    077 - 9.8 - видео 9-8: 05:50

---
+++ 9.0/9.8/dom-logic.js
@@ -0,0 +1,73 @@
+
+// document.addEventListener("DOMContentLoaded", function () {
+// });
+
+const opennedFilms = {};
+
+document.addEventListener("DOMContentLoaded", function () {
+    for (let category of categories) {
+        const newEl = document.createElement("div");
+        newEl.classList.add("category");
+        newEl.innerText = category;
+        newEl.addEventListener("click", function () {
+            onCategoryChoice(category);
+        });
+        document.querySelector(".categories").appendChild(newEl);
+    }
+});
+
+function getFilmComments(filmName) {
+    const film = films.filter( f => f.name === filmName )[0];
+    return film.comments;
+}
+
+function renderCommentForm(film) {
+    
+}
+
+function openFilmCard(film, newEl) {
+    const comments = getFilmComments(film.name);
+    let s = "";
+    comments.forEach(c => {
+        s+= `<div class="film-comment"><span class="comment-author">${c.author}</span>: ${c.text}</div>`;
+    });
+    newEl.innerHTML +=`<div class="film-comments">${s}</div>`;
+    const addCommentButton = document.createElement("button");
+    addCommentButton.innerText = "Добавить отзыв";
+    addCommentButton.addEventListener("click", function (event) {
+        event.stopPropagation();
+        const commentForm = renderCommentForm(film);
+        newEl.appendChild(commentForm);
+    });
+    newEl.appendChild(addCommentButton);
+}
+
+function onFilmClick(film, newEl) {
+    if (opennedFilms.hasOwnProperty( film.name ) && opennedFilms[film.name] ){
+        newEl.innerHTML =`<div class="film-name">${film.name}</div>`;
+        opennedFilms[film.name] = false;
+        // return ;
+    } else {
+        openFilmCard(film, newEl);
+        opennedFilms[film.name] = true;
+    }
+
+}
+
+function renderFilm(film) {
+    const newEl = document.createElement("div");
+    newEl.classList.add("film");
+    newEl.innerHTML =`<div class="film-name">${film.name}</div>`;
+    newEl.addEventListener("click", function () {
+        onFilmClick(film, newEl);
+    });
+    document.querySelector(".films").appendChild(newEl);
+}
+
+function onCategoryChoice(categoryName){
+    document.querySelector(".films").innerHTML="";
+    const films = getFilmsByCategory(categoryName);
+    for (let film of films) {
+        renderFilm(film);
+    }
+}
